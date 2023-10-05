61fbe14a9c0c91ea68732ac2309a6eb2de5423d0
Jack Parker <jparke10@uoregon.edu>
2023-10-05 16:06:09 -0700

    wrote boilerplate code for lab2

---
+++ lab/lab2/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "command.h"
+
+#define _GNU_SOURCE
+
+int main(int argc, char** argv)
+{
+	(void) argv;
+	//checking for command line argument
+	if (argc != 1)
+	{
+		printf ("Usage: [path to executable from directory to list]\n");
+	}
+
+	//declear line_buffer
+	size_t len = 128;
+	char* line_buf = malloc (len);
+    printf(">>> ");
+	//loop until the file is over
+	while (getline (&line_buf, &len, stdin) != -1)
+	{
+        if (strcmp(line_buf, "lfcat\n") == 0) {
+            lfcat();
+            printf(">>> ");
+        } else if (strcmp(line_buf, "exit\n") == 0)
+            break;
+        else {
+            printf("Error: Unrecognized command!\n");
+            printf(">>> ");
+        }
+	}
+    free(line_buf);
+}
