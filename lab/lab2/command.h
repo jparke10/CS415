e0eaf025a768c77572798877967c67dff77d55aa
Jack Parker <jparke10@uoregon.edu>
2023-10-05 16:05:55 -0700

    starter code for lab2

---
+++ lab/lab2/command.h
@@ -0,0 +1,10 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <string.h>
+
+void lfcat();
