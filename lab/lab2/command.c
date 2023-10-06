25ae8690afd5b93c862ad085b33a0f4661bb0ca2
Jack Parker <jparke10@uoregon.edu>
2023-10-06 10:19:13 -0700

    finished lfcat implementation

--- lab/lab2/command.c
+++ lab/lab2/command.c
@@ -6,21 +6,26 @@ void lfcat()
 	/* Get the current directory with getcwd() */
 	char* path_buf = malloc(PATH_MAX);
 	char* pathname = getcwd(path_buf, PATH_MAX);
+	// construct dash delimiting string for command.c:42
+	char* dash = malloc(82);
+	dash[0] = '\n';
+	memset(dash + 1, '-', 80);
+	dash[81] = '\n';
 	/* Open the dir using opendir() */
 	DIR* dp;
 	dp = opendir(pathname);
-	/* use a while loop to read the dir with readdir()*/
 	struct dirent* dirp;
-	// printf("Listing all files in the current directory\n");
 	/* redirect stdout to file output */
 	freopen("output.txt", "w", stdout);
+	// printf("Listing all files in the current directory\n");
+	/* use a while loop to read the dir with readdir()*/
 	while ((dirp = readdir(dp)) != NULL) {
 		/* You can debug by printing out the filenames here */
 		// if (dirp->d_type == DT_REG)
 		//     printf("File: %s\n", dirp->d_name);	
 		/* Option: use an if statement to skip any names that are not readable files (e.g. ".", "..", "main.c", "lab2.exe", "output.txt" */
-		if (dirp->d_type == DT_REG) {
-			fprintf(stdout, "File: %s\n", dirp->d_name);
+		if (dirp->d_type == DT_REG &&
+			strcmp(dirp->d_name, "output.txt") != 0) {
 			char* line_buf = NULL;
 			size_t n = 0;
 			/* Open the file */
@@ -30,19 +35,17 @@ void lfcat()
 			/* Read in each line using getline() */
 			while (getline(&line_buf, &n, file_in) != EOF) {
 				/* Write the line to stdout */
-				write(STDOUT_FILENO, "test\n", 5);
+				write(STDOUT_FILENO, line_buf, strlen(line_buf));
 			}
 			/* write 80 "-" characters to stdout */
-			char* dash = malloc(82);
-			memset(dash, '-', 80);
-			memset(dash + 80, '\n', 1);
-			write(STDOUT_FILENO, dash, 81);
-			free(dash);
+			write(STDOUT_FILENO, dash, 82);
 			/* close the read file and free/null assign your line buffer */
 			free(line_buf);
 			fclose(file_in);
 		}
 	}
+	free(dash);
+	// redirect stdout back to unix console
 	fclose(stdout);
 	stdout = fdopen(0, "w");
 	/*close the directory you were reading from using closedir() */
