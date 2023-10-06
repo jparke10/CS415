72ddeeccdf6a3626fefab9a184e2ca6b838e0375
Jack Parker <jparke10@uoregon.edu>
2023-10-06 12:27:22 -0700

    boilerplate code for pseudoshell commands

---
+++ project1/command.c
@@ -0,0 +1,42 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <string.h>
+#include "string_parser.h"
+#include "command.h"
+
+void listDir() {
+
+}
+
+void showCurrentDir() {
+
+}
+
+void makeDir(char *dirName) {
+
+}
+
+void changeDir(char *dirName) {
+
+}
+
+void copyFile(char *sourcePath, char *destinationPath) {
+
+}
+
+void moveFile(char *sourcePath, char *destinationPath) {
+
+}
+
+void deleteFile(char *filename) {
+
+}
+
+void displayFile(char *filename) {
+
+}
