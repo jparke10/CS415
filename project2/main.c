4720c3f7f7c1e1f9149301bd540117682bbb8445
Jack Parker <jparke10@uoregon.edu>
2023-10-24 11:40:29 -0700

    more boilerplate for project2 part 1

--- project2/main.c
+++ project2/main.c
@@ -18,16 +18,29 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Error: could not open file %s\n", argv[1]);
         exit(EXIT_FAILURE);
     }
-    command_line lines, args;
+    command_line args;
     char* buf;
     size_t buf_size;
+
     fseek(file_in, 0, SEEK_END);
     buf_size = ftell(file_in);
     fseek(file_in, 0, SEEK_SET);
     buf = malloc(buf_size);
     fread(buf, sizeof(char), buf_size, file_in);
     size_t num_lines = count_token(buf, "\n") - 1;
-    printf("%d\n", num_lines);
+    free(buf);
+    fseek(file_in, 0, SEEK_SET);
+
     pid_t* pid_array = malloc(sizeof(pid_t) * num_lines);
+    buf = malloc(LINE_MAX);
+    buf_size = LINE_MAX;
+    for (int i = 0; i < num_lines; i++) {
+        getline(&buf, &buf_size, file_in);
+        args = str_filler(buf, " ");
+        for (int j = 0; args.command_list[j] != NULL; j++) {
+        }
+    }
+    free(buf);
+    fclose(file_in);
     exit(EXIT_SUCCESS);
 }
