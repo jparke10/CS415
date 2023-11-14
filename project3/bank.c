d6a1b43801f7953a027525bd1e5a84a702878c71
Jack Parker <jparke10@uoregon.edu>
2023-11-14 08:46:36 -0800

    project 3 boilerplate

--- project3/bank.c
+++ project3/bank.c
@@ -4,13 +4,47 @@
 #include <limits.h>
 #include <string.h>
 #include "account.h"
+#include "string_parser.h"
 
-void* process_transaction(void* arg) {
+unsigned int num_accounts = 0;
+account* account_array;
 
+void* process_transaction(command_line* arg) {
+    char* type = arg->command_list[0];
+    char* account = arg->command_list[1];
+    char* password = arg->command_list[2];
+    int account_index = -1;
+    for (int i = 0; i < num_accounts; i++) {
+        if (strcmp(account, account_array[i].account_number) == 0) {
+            account_index = i;
+            break;
+        }
+    }
+    // password validation
+    if (strcmp(password, account_array[account_index].password) != 0) {
+        return;
+    }
+
+    if (type == "T") {
+
+    } else if (type == "C") {
+
+    } else if (type == "D") {
+
+    } else if (type == "w") {
+
+    }
 }
 
 void* update_balance(void* arg) {
 
+}
+
+void usage(int argc, char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s [input TXT file]\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
 }
 
 int main(int argc, char** argv) {
@@ -25,22 +59,24 @@ int main(int argc, char** argv) {
     char* buf = malloc(buf_size);
 
     getline(&buf, &buf_size, file_in);
-    const unsigned int num_accounts = atoi(buf);
+    num_accounts = atoi(buf);
     if (num_accounts == 0) {
         fprintf(stderr, "Error: Invalid number of accounts in input file\n");
         exit(EXIT_FAILURE);
     }
 
-    account* account_array = malloc(sizeof(account) * num_accounts);
+    account_array = (account*)malloc(sizeof(account) * num_accounts);
     for (int i = 0; i < num_accounts; i++) {
         // index line
         getline(&buf, &buf_size, file_in);
         // account number line
         getline(&buf, &buf_size, file_in);
-        strncpy(account_array[i].account_number, buf, 17);
+        strncpy(account_array[i].account_number, buf, 16);
+        strncat(account_array[i].account_number, "\0", 1);
         // password line
         getline(&buf, &buf_size, file_in);
-        strncpy(account_array[i].password, buf, 9);
+        strncpy(account_array[i].password, buf, 8);
+        strncat(account_array[i].password, "\0", 1);
         // initial balance
         getline(&buf, &buf_size, file_in);
         account_array[i].balance = atof(buf);
@@ -49,12 +85,11 @@ int main(int argc, char** argv) {
         account_array[i].reward_rate = atof(buf);
     }
 
-    
-}
-
-void usage(int argc, char** argv) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s [input TXT file]\n", argv[0]);
-        exit(EXIT_FAILURE);
+    command_line transaction;
+    while (getline(&buf, &buf_size, file_in) != EOF) {
+        transaction = str_filler(buf, " ");
+        process_transaction(&transaction);
+        free_command_line(&transaction);
+        memset(&transaction, 0, 0);
     }
 }
