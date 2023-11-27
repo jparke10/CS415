9b5169a443126f029f69311af7310f85297a5c9e
Jack Parker <jparke10@uoregon.edu>
2023-11-27 11:23:43 -0800

    finished part 1, updated folder structure

--- project3/part1/bank.c
+++ project3/part1/bank.c
@@ -9,7 +9,7 @@
 #include "string_parser.h"
 
 unsigned int num_accounts = 0;
-account* account_array;
+account* account_array = NULL;
 
 void* process_transaction(void* arg) {
     command_line* input = (command_line*)arg;
@@ -28,7 +28,7 @@ void* process_transaction(void* arg) {
         return NULL;
     }
 
-    if (type == "T") {
+    if (strcmp(type, "T") == 0) {
         int destination_index = -1;
         double amount = atof(input->command_list[4]);
         for (int i = 0; i < num_accounts; i++) {
@@ -38,22 +38,28 @@ void* process_transaction(void* arg) {
             }
         }
         account_array[account_index].balance -= amount;
+        account_array[account_index].transaction_tracter += amount;
         account_array[destination_index].balance += amount;
-    } else if (type == "C") {
-        FILE* out = fopen(account_array[account_index].out_file, "w");
-        fprintf(out, "Current Balance:\t%d\n", account_array[account_index].balance);
-        fclose(out);
-    } else if (type == "D") {
+    } else if (strcmp(type, "C") == 0) {
+    } else if (strcmp(type, "D") == 0) {
         double amount = atof(input->command_list[3]);
         account_array[account_index].balance += amount;
-    } else if (type == "w") {
+        account_array[account_index].transaction_tracter += amount;
+    } else if (strcmp(type, "W") == 0) {
         double amount = atof(input->command_list[3]);
         account_array[account_index].balance -= amount;
+        account_array[account_index].transaction_tracter += amount;
     }
+    return NULL;
 }
 
 void* update_balance(void* arg) {
-
+    for (int i = 0; i < num_accounts; i++) {
+        account_array[i].balance += account_array[i].transaction_tracter *
+                                    account_array[i].reward_rate;
+        FILE* acc = fopen(account_array[i].out_file, "a");
+        fprintf(acc, "Current Balance:\t%.2f\n", account_array[i].balance);
+    }
 }
 
 void usage(int argc, char** argv) {
@@ -72,7 +78,7 @@ int main(int argc, char** argv) {
     }
 
     size_t buf_size = LINE_MAX;
-    char* buf = malloc(buf_size);
+    char* buf = (char*)malloc(buf_size);
 
     getline(&buf, &buf_size, file_in);
     num_accounts = atoi(buf);
@@ -80,18 +86,28 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Error: Invalid number of accounts in input file\n");
         exit(EXIT_FAILURE);
     }
+    printf("DuckBank accounts: %d\n", num_accounts);
 
     account_array = (account*)malloc(sizeof(account) * num_accounts);
-    mkdir("out", 0777);
+    printf("Populating accounts...");
+    mkdir("output", 0777);
     for (int i = 0; i < num_accounts; i++) {
+        // null initializations of each account
+        // helps prevent memory errors
+        memset(account_array[i].account_number, 0, 17);
+        memset(account_array[i].password, 0, 9);
+        memset(account_array[i].out_file, 0, 64);
+        account_array[i].balance = 0.;
+        account_array[i].reward_rate = 0.;
+        account_array[i].transaction_tracter = 0.;
+        pthread_mutex_init(&(account_array[i].ac_lock), NULL);
         // index line
         getline(&buf, &buf_size, file_in);
         // account number line
         getline(&buf, &buf_size, file_in);
         strncpy(account_array[i].account_number, buf, 16);
-        snprintf(account_array[i].out_file, 63, "./out/%s.txt",
+        snprintf(account_array[i].out_file, 63, "./output/%s.txt",
                  account_array[i].account_number);
-        strncat(account_array[i].account_number, "\0", 1);
         FILE* acc = fopen(account_array[i].out_file, "w");
         fprintf(acc, "account %d:\n", i);
         fclose(acc);
@@ -106,12 +122,27 @@ int main(int argc, char** argv) {
         getline(&buf, &buf_size, file_in);
         account_array[i].reward_rate = atof(buf);
     }
+    printf("done\n");
 
     command_line transaction;
+    printf("Processing transactions from %s...", argv[1]);
     while (getline(&buf, &buf_size, file_in) != EOF) {
         transaction = str_filler(buf, " ");
         process_transaction(&transaction);
         free_command_line(&transaction);
         memset(&transaction, 0, 0);
     }
+    printf("done\n");
+
+    printf("Updating balances with reward rate...");
+    update_balance(NULL);
+    printf("done\n");
+
+    FILE* transactions = fopen("output.txt", "w");
+    for (int i = 0; i < num_accounts; i++) {
+        fprintf(transactions, "%d balance:\t%.2f\n\n", i, account_array[i].balance);
+    }
+
+    fclose(transactions);
+    free(buf);
 }
